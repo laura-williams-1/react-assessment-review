04e8c24b06610f954ef4aab6290a5427ee451c94
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-22 19:02:50 -0400

    Nav Links added and correctred container import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./Components/Header";
-import Container from "./Components/Header";
+import Container from "./Container";
 function App() {
   return (
     <div className="App">
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,23 @@
+import { NavLink } from "react-router-dom";
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <h3>Wyze</h3>
+      <ul>
+        <NavLink to="/">
+          <li>Home</li>
+        </NavLink>
+
+        <NavLink to="/random">
+          <li>Random Advice</li>
+        </NavLink>
+        <NavLink to="/search">
+          <li>Search</li>
+        </NavLink>
+        <NavLink to="/aboutus">
+          <li>About Us</li>
+        </NavLink>
+      </ul>
+    </header>
   );
 }
