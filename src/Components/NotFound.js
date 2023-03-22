e7e2433a503ff489955d8cd7490a7200157d7728
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-22 19:07:38 -0400

    added to error message

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,7 +1,8 @@
 export default function NotFound() {
   return (
     <div>
-      <h2>404</h2>
+      <h1>404</h1>
+      <h3>Sorry the link you are looking for can't be found</h3>
     </div>
   );
 }
